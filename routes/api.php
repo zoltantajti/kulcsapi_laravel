d349ed2982320ffac62bba3e6e88b56f0afc25ca
zoltantajti <zoltan.tajti92@gmail.com>
2025-08-07 20:50:42 +0200

    skeleton ok, middleware/sanctum fail

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/status', function() { return response()->json(["status" => "ok"]); });
+Route::any('/teszt', function(Request $request) {
+    $authHeader = $request->header('Authorization');
+    return response()->json([
+        'msg' => 'Végpont',
+        'auth' => $authHeader,
+        'fejlec' => $request->headers->all()
+    ]);
+});
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request){
         return $request->user();
     });
     Route::post('/validate', [LicenseController::class, 'validateKey']);
-    Route::post('/use', [LicenseController::class, 'useKey']);    
+    Route::post('/use', [LicenseController::class, 'useKey']);
 });
